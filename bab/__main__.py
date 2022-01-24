74846a194722643d5ee743c6ca3d77f1810be437
Andreas Rizk <andreasrizk8@gmail.com>
2022-01-24 17:44:59 -0500

    Added requirements.txt for required libraries and a default Python bot using Hikari-Lightbulb with necessary directories for extensions

---
+++ bab/__main__.py
@@ -0,0 +1,4 @@
+from bab import bot
+
+if __name__ == "__main__":
+    bot.run()
---
+++ bab/bot.py
@@ -0,0 +1,12 @@
+import os
+import lightbulb
+
+bot = lightbulb.BotApp(
+    os.environ["TOKEN"],
+    default_enabled_guilds=int(os.environ["GUILD_ID"]),
+    help_slash_command=True,
+)
+
+def run():
+    bot.load_extensions_from("./bab/active_extensions")
+    bot.run()
