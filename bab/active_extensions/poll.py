73b59288b2f5be4fe2ac6916e95a429287a4647e
Andreas Rizk <rizka@rpi.edu>
2022-03-23 23:34:33 -0400

    added poll command

--- bab/active_extensions/poll.py
+++ bab/active_extensions/poll.py
@@ -1,84 +1,22 @@
 import hikari
 import lightbulb
-import pickle
-import random
-from os.path import exists
-
-############################# CLASS ############################################
-
-# Game class which holds all the data for each poll game
-class Poll:
-    def __init__(self, user, text):
-        self.user = user
-        self.text = text
-        self.yay = 0
-        self.nae = 0
-
-############################# GLOBALS AND DATA MANAGEMENT ############################################
-
-polls = {}
-
-if (exists("bab/active_extensions/data/polls")):
-    with open("bab/active_extensions/data/polls","rb") as f:
-        polls = pickle.load(f)      
-
-def save(): 
-    with open("bab/active_extensions/data/polls","wb") as f:
-        pickle.dump(polls,f)
-
-############################# LIGHTBULB PLUGIN COMMANDS ############################################
 
 plugin = lightbulb.Plugin("poll")
 
-@plugin.command # Main poll command group
+@plugin.command 
+@lightbulb.option("prompt","Poll prompt.", str, required=True)
 @lightbulb.command("poll", "A poll")
-@lightbulb.implements(lightbulb.SlashCommandGroup,lightbulb.PrefixCommandGroup)
+@lightbulb.implements(lightbulb.SlashCommand,lightbulb.PrefixCommand)
 async def poll(ctx: lightbulb.MessageContext)->None:
-    pass
-
-################# COMMANDS WITHIN GROUP #########################
-
-def poll_id():
-    x = random.randint(1,100)
-    while x in polls:
-        x = random.randint(1,100)
-    return x
-        
-@poll.child
-@lightbulb.option("text","The text describing your poll.", str, required=True)
-@lightbulb.command("create", "Create a poll.")
-@lightbulb.implements(lightbulb.SlashSubCommand,lightbulb.PrefixSubCommand)
-async def play(ctx: lightbulb.MessageContext)->None:
-        id = poll_id()
-        polls[id] = poll(ctx.user.id,ctx.options.text)
         embed = (
-            hikari.Embed(title=f"{ctx.options.text}", description=f"Poll create by {ctx.user.name}")
-            .add_field(f"Poll id is {id}", "")
-            .set_footer(f"{ctx.user}'s poll")
+        hikari.Embed(title=f"{ctx.options.prompt}", description=f"Poll create by {ctx.user}")
+        .set_footer("Build-A-Bot Poll")
         )
-        await ctx.respond(embed)
+        rp = await ctx.respond(embed)
+        msg = await rp.message()
+        await msg.add_reaction("✅")
+        await msg.add_reaction("❌")
 
-@poll.child
-@lightbulb.option("id","ID of the pole you want to vote on.", int, required=True)
-@lightbulb.option("vote","Vote yea or nay.", str, required=True)
-@lightbulb.command("vote", "vote on a poll")
-@lightbulb.implements(lightbulb.SlashSubCommand,lightbulb.PrefixSubCommand)
-async def guess(ctx: lightbulb.MessageContext)->None:
-    pass
-
-@poll.child
-@lightbulb.option("id","ID of the pole you want to end.", int, required=True)
-@lightbulb.command("end", "End your poll.")
-@lightbulb.implements(lightbulb.SlashSubCommand,lightbulb.PrefixSubCommand)
-async def end(ctx: lightbulb.MessageContext)->None:
-    if not ctx.user.id == polls[ctx.options.id].user:
-        ctx.user.reply("You cannot end a poll you did not start!")
-        return
-    polls.pop(ctx.options.id)
-
-
-
-############################# LOADING AND UNLOADING THE PLUGIN ############################################
 def load(bot: lightbulb.BotApp) -> None:
     bot.add_plugin(plugin)
 
